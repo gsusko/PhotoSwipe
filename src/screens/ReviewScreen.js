f264bce8c6d7db988d3bddf504add89a96d547bd
Greg Susko <gsusko1@gmail.com>
2017-12-21 13:51:06 -0800

    add settings nav on review header

--- src/screens/ReviewScreen.js
+++ src/screens/ReviewScreen.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
+  static navigationOptions = ({ navigation }) => {
+    const { navigate } = navigation;
+    return {
+      title: 'Review Photos',
+      headerRight: (
+        <Button
+          title="Settings"
+          onPress={() => navigate('settings')}
+          backgroundColor="rgba(0,0,0,0)"
+          color="rgba(0, 122, 255, 1)"
+        />
+      )
+    };
+  }
 
   render() {
     return (
