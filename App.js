f85efac51e5c88c79532cee4a188208e18e7eeb0
Greg Susko <gsusko1@gmail.com>
2017-12-21 13:48:47 -0800

    add screen boilerplate and tab nav

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+
+import AuthScreen from './src/screens/AuthScreen';
+import WelcomeScreen from './src/screens/WelcomeScreen';
+import SearchScreen from './src/screens/SearchScreen';
+import DeckScreen from './src/screens/DeckScreen';
+import ReviewScreen from './src/screens/ReviewScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
 
 export default class App extends React.Component {
   render() {
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          search: { screen: SearchScreen },
+          deck: { screen: DeckScreen },
+          review: {
+            screen: StackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            })
+          }
+        })
+      },
+    });
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <MainNavigator />
     );
   }
 }
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class SearchScreen extends Component {
+
+  render() {
+    return (
+      <View>
+        <Text>Search Screen</Text>
+        <Text>Search Screen</Text>
+        <Text>Search Screen</Text>
+        <Text>Search Screen</Text>
+        <Text>Search Screen</Text>
+      </View>
+    );
+  }
+
+}
+
+export default SearchScreen;
