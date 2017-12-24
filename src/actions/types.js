634c83126867a3ca1a17d871e43b7a8c72409b55
Greg Susko <gsusko1@gmail.com>
2017-12-23 22:04:21 -0800

    update to use facebook login rather than instagram

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,2 @@
-export const IG_LOGIN_SUCCESS = 'ig_login_success';
-export const IG_LOGIN_FAIL = 'ig_login_fail';
+export const FB_LOGIN_SUCCESS = 'fb_login_success';
+export const FB_LOGIN_FAIL = 'fb_login_fail';
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+import axios from 'axios';
+import { connect } from 'react-redux';
 
 class SearchScreen extends Component {
+
+  async onButtonPress() {
+    let pics = await axios.get('http://api.flickr.com/services/feeds/photos_public.gne?tags=dog,smart&format=json&nojsoncallback=true');
+
+    console.log(pics)
+
+  }
 
   render() {
     return (
       <View>
-        <Text>Search Screen</Text>
-        <Text>Search Screen</Text>
-        <Text>Search Screen</Text>
-        <Text>Search Screen</Text>
-        <Text>Search Screen</Text>
+        <Button
+          style={{marginTop: 50}}
+          title="Search"
+          onPress={() => this.onButtonPress()}
+        />
       </View>
     );
   }
-
 }
 
-export default SearchScreen;
+mapStateToProps = (state) => {
+  return {
+    token: state.auth.token
+  }
+}
+
+export default connect(mapStateToProps)(SearchScreen);
